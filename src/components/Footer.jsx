d6ab12a98419bffdfe5c93f1f3d6f1c984d86de0
Kamal Azizov <70916892+Rungrukuk@users.noreply.github.com>
2026-07-29 08:35:47 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
         <div className="footer-content">
             <p>© 2026 Kamal Azizov. All Rights Reserved.</p>
             <p>
-                <a href="https://www.linkedin.com/in/kamal-%C9%99zizov-6222781a4/" target="_blank" rel="noopener noreferrer">LinkedIn</a> |
+                <a href="https://www.linkedin.com/in/kamal-azizov-6222781a4" target="_blank" rel="noopener noreferrer">LinkedIn</a> |
                 <a href="https://github.com/Rungrukuk" target="_blank" rel="noopener noreferrer">GitHub</a> |
                 <a href="mailto:kamilo09ya@gmail.com">Email</a>
             </p>
